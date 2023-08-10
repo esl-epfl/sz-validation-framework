f2ea4f903a925a319358f8f63fa581a26f183eb9
Jonathan Dan <jonathan.dan@epfl.ch>
2023-08-10 12:38:48 +0200

    Push files

---
+++ loadAnnotations/sienaAnnotationConverter.py
@@ -0,0 +1,317 @@
+"""Script to load annotations from the Siena dataset https://doi.org/10.13026/5d4a-j060
+to the standardized annotation format.
+
+The annotation format is described in more detail on
+https://eslweb.epfl.ch/epilepsybenchmarks/framework-for-validation-of-epileptic-seizure-detection-algorithms/#annotation
+
+The script can be used as a library or as a command line application.
+"""
+
+import argparse
+import glob
+import os
+from pathlib import Path
+import re
+import time
+
+import pandas as pd
+import pyedflib
+
+
+def _parseTimeStamp(string: str) -> float:
+    """Parses timestamps from Siena annotation files and returns a float representing the time from the earliest system time.
+
+    Accepts the following formats with any random string before the timestamp :
+    - 'Registration start time: 19.00.44'
+    - 'Start time: 21:51:02'
+
+    Args:
+        string (str): string to be parsed as a timestamp
+
+    Returns:
+        float: timestamp in seconds from the earliest system time
+    """
+    timeStamp = re.findall('.*[ |:]([0-9]+.[0-9]+.[0-9]+).*', string)[0]
+    return time.mktime(time.strptime(timeStamp.replace(':', '.'), "%H.%M.%S"))
+
+
+def _substractTimeStamps(time1: float, time2: float) -> float:
+    """Substract to timestamps in seconds"""
+    difference = time1 - time2
+    if difference < 0:
+        difference += 24 * 60 * 60  # Correct for day shift
+    return difference
+
+
+def _loadSeizures(edfFile: str, subject: str, correctedEdfFileName: str) -> list[tuple]:
+    """Load seizures from a Siena Seizures-list-Pxx.txt file"""
+    seizures = []
+    summaryFile = os.path.join(os.path.dirname(edfFile), 'Seizures-list-{}.txt'.format(subject))
+    with open(summaryFile, 'r') as summary:
+        # Search for mention of edfFile in summary
+        line = summary.readline()
+        while line:
+            if re.search(r'File name: {} *\n'.format(correctedEdfFileName), line):
+                firstLine = summary.readline()
+                # PN01 exception
+                if correctedEdfFileName == 'PN01.edf':
+                    registrationStart = _parseTimeStamp(firstLine)
+                    _ = summary.readline()
+                    _ = summary.readline()
+                    for _ in range(2):
+                        _ = summary.readline()
+                        start = _parseTimeStamp(summary.readline())
+                        end = _parseTimeStamp(summary.readline())
+                        _ = summary.readline()
+                        seizures.append((_substractTimeStamps(start, registrationStart),
+                                        (_substractTimeStamps(end, registrationStart))))
+                # PN12 exception
+                elif 'Seizure start time: ' in firstLine:
+                    start = _parseTimeStamp(firstLine)
+                    end = _parseTimeStamp(summary.readline())
+                    seizures.append((_substractTimeStamps(start, registrationStart),
+                                    (_substractTimeStamps(end, registrationStart))))
+                # Standard format
+                elif 'Registration start time:' in firstLine:
+                    registrationStart = _parseTimeStamp(firstLine)
+                    _ = summary.readline()
+                    start = _parseTimeStamp(summary.readline())
+                    end = _parseTimeStamp(summary.readline())
+                    seizures.append((_substractTimeStamps(start, registrationStart),
+                                    (_substractTimeStamps(end, registrationStart))))
+                else:
+                    raise ValueError("Unknown format for Siena summary file.")
+            line = summary.readline()
+    return seizures
+
+
+def _correctEdfFileNameTypos(filename: str) -> str:
+    """Correct typos in filenames in .txt file when compared to .edf files"""
+    correctedEdfFileName = os.path.basename(filename)
+    if correctedEdfFileName == 'PN01-1.edf':
+        correctedEdfFileName = 'PN01.edf'
+    elif correctedEdfFileName == 'PN06-1.edf':
+        correctedEdfFileName = 'PNO6-1.edf'  # big o instead of 0
+    elif correctedEdfFileName == 'PN06-2.edf':
+        correctedEdfFileName = 'PNO6-2.edf'  # big o instead of 0
+    elif correctedEdfFileName == 'PN06-4.edf':
+        correctedEdfFileName = 'PNO6-4.edf'  # big o instead of 0
+    elif correctedEdfFileName == 'PN11-1.edf':
+        correctedEdfFileName = 'PN11-.edf'
+    return correctedEdfFileName
+
+
+def convertAnnotationsEdf(rootDir: str, edfFile: str, outFile: str = None) -> pd.DataFrame:
+    """Loads annotations related to an EDF recording in the Siena dataset. The annotations are returned as a pandas DataFrame
+        and optionally written to a csv file.
+
+    Args:
+        rootDir (str): root directory of the Siena dataset. This refers to the location containing the file subject_info.csv
+            and the folders for each subject.
+        edfFile (str): full path to the EDF for which annotations should be extracted.
+        outFile (str, optional): full path to the csv file to which the annotations will be written. If it is set to None no
+            annotations will be written. Defaults to None.
+
+    Raises:
+        ValueError: raised if the seizure type is unknown in the subject_info.csv file.
+        ValueError: raised if the format of an annotation is unknown.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the annotations for each seizure the following fields are give : subject, session,
+            recording, recording start dateTime, recording duration, seizure type, event start time in seconds relative to the
+            start of the recording, event end time [s], confidence=1, channels, filepath
+    """
+
+    annotations = {
+        'subject': [],
+        'session': [],
+        'recording': [],
+        'dateTime': [],
+        'duration': [],
+        'event': [],
+        'startTime': [],
+        'endTime': [],
+        'confidence': [],
+        'channels': [],
+        'filepath': []
+    }
+
+    subject_info = pd.read_csv(os.path.join(rootDir, 'subject_info.csv'))
+
+    # Correct typos in filenames in .txt file when compared to .edf files
+    correctedEdfFileName = _correctEdfFileNameTypos(edfFile)
+
+    # Subject
+    subject = edfFile.split('/')[-2]
+
+    # Session
+    fileName = Path(edfFile).stem
+    session = fileName.split('-')
+    if len(session) == 0:
+        session = 1
+    else:
+        session = session[-1]
+
+    # Recording
+    recording = 1
+
+    # dateTime and duration
+    with pyedflib.EdfReader(edfFile) as edf:
+        dateTime = edf.getStartdatetime()
+        duration = edf.getFileDuration()
+        edf._close()
+
+    # Get Seizure type
+    seizureType = subject_info[subject_info.patient_id == subject][' seizure'].iloc[0]
+    if seizureType == 'IAS':
+        seizureType = 'sz-foc-ia'
+    elif seizureType == 'WIAS':
+        seizureType = 'sz-foc-ia'
+    elif seizureType == 'FBTC':
+        seizureType = 'sz-foc-ua-f2b'
+    else:
+        raise ValueError("Unknown seizure type for Siena dataset.")
+
+    # Load Seizures
+    seizures = _loadSeizures(edfFile, subject, correctedEdfFileName)
+
+    # Confidence
+    confidence = 1
+
+    # Channels
+    channels = 'all'  # TODO use lateralization info to populate channels
+
+    # Filepath
+    filepath = subject + '/' + os.path.basename(edfFile)
+
+    # Populate dictionary
+    if len(seizures) == 0:
+        seizureType = 'bckg'
+        seizures.append((0, duration))
+
+    for seizure in seizures:
+        annotations['subject'].append(subject)
+        annotations['session'].append(session)
+        annotations['recording'].append(recording)
+        annotations['dateTime'].append(dateTime)
+        annotations['duration'].append(duration)
+        annotations['event'].append(seizureType)
+        annotations['startTime'].append(int(seizure[0]))
+        annotations['endTime'].append(int(seizure[1]))
+        annotations['confidence'].append(confidence)
+        annotations['channels'].append(channels)
+        annotations['filepath'].append(filepath)
+
+    annotationDf = pd.DataFrame(annotations)
+
+    if outFile is not None:
+        annotationDf.sort_values(by=['filepath']).to_csv(outFile, index=False)
+
+    return annotationDf
+
+
+def convertAnnotationsSubject(rootDir: str, subject: str, outFile: str = None) -> pd.DataFrame:
+    """Loads annotations related to a subject in the Siena dataset. The annotations are returned as a pandas DataFrame
+        and optionally written to a csv file.
+
+    Args:
+        rootDir (str): root directory of the Siena dataset. This refers to the location containing the file subject_info.csv
+            and the folders for each subject.
+        subject (str): name of the subject in the Siena dataset (e.g. PN00)
+        outFile (str, optional): full path to the csv file to which the annotations will be written. If it is set to None no
+            annotations will be written. Defaults to None.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the annotations for each seizure the following fields are give : subject, session,
+            recording, recording start dateTime, recording duration, seizure type, event start time in seconds relative to the
+            start of the recording, event end time [s], confidence=1, channels, filepath
+    """
+    annotations = {
+        'subject': [],
+        'session': [],
+        'recording': [],
+        'dateTime': [],
+        'duration': [],
+        'event': [],
+        'startTime': [],
+        'endTime': [],
+        'confidence': [],
+        'channels': [],
+        'filepath': []
+    }
+    annotationDf = pd.DataFrame(annotations)
+
+    edfFiles = glob.iglob(os.path.join(rootDir, subject, '*.edf'))
+    for edfFile in edfFiles:
+        edfAnnotations = convertAnnotationsEdf(rootDir, edfFile)
+        annotationDf = pd.concat([annotationDf, edfAnnotations])
+
+    if outFile is not None:
+        annotationDf.sort_values(by=['filepath']).to_csv(outFile, index=False)
+
+    return annotationDf
+
+
+def convertAllAnnotations(rootDir: str, outFile: str = None) -> pd.DataFrame:
+    """Loads all annotations in the Siena dataset. The annotations are returned as a pandas DataFrame and optionally written
+        to a csv file.
+
+    Args:
+        rootDir (str): root directory of the Siena dataset. This refers to the location containing the file subject_info.csv
+            and the folders for each subject.
+        outFile (str, optional): full path to the csv file to which the annotations will be written. If it is set to None no
+            annotations will be written. Defaults to None.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the annotations for each seizure the following fields are give : subject, session,
+            recording, recording start dateTime, recording duration, seizure type, event start time in seconds relative to the
+            start of the recording, event end time [s], confidence=1, channels, filepath
+    """
+    annotations = {
+        'subject': [],
+        'session': [],
+        'recording': [],
+        'dateTime': [],
+        'duration': [],
+        'event': [],
+        'startTime': [],
+        'endTime': [],
+        'confidence': [],
+        'channels': [],
+        'filepath': []
+    }
+    annotationDf = pd.DataFrame(annotations)
+
+    edfFiles = glob.iglob(os.path.join(rootDir, '*/*.edf'))
+    for edfFile in edfFiles:
+        edfAnnotations = convertAnnotationsEdf(rootDir, edfFile)
+        annotationDf = pd.concat([annotationDf, edfAnnotations])
+
+    if outFile is not None:
+        annotationDf.sort_values(by=['filepath']).to_csv(outFile, index=False)
+
+    return annotationDf
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='Siena Annotation converter',
+        description='Converts annatations from the Siena dataset to a standardized format',
+    )
+    parser.add_argument('rootDir',
+                        help="root directory of the Siena dataset. This refers to the location containing the file "
+                             "subject_info.csv and the folders for each subject.")
+    parser.add_argument('outFile',
+                        help="full path to the csv file to which the annotations will be written.")
+    parser.add_argument('-s', '--subject',
+                        help="If provided, only extracts the annotations from the subject.")
+    parser.add_argument('-e', '--edf',
+                        help="If provided, only extracts the annotations from the specified EDF file. Expects a full path.")
+    args = parser.parse_args()
+
+    if args.edf is not None:
+        convertAnnotationsEdf(args.rootDir, args.edf, args.outFile)
+    elif args.subject is not None:
+        convertAnnotationsSubject(args.rootDir, args.subject, args.outFile)
+    else:
+        convertAllAnnotations(args.rootDir, args.outFile)
---
+++ loadEeg/loadEdf.py
@@ -0,0 +1,218 @@
+"""Script to convert EDF data to a standardized data format.
+
+The data format is described in more detail on
+https://eslweb.epfl.ch/epilepsybenchmarks/framework-for-validation-of-epileptic-seizure-detection-algorithms/#eegformat
+
+The script can be used as a library or as a command line application.
+"""
+import argparse
+import os
+import glob
+import re
+
+import numpy as np
+import pyedflib
+import resampy
+
+ELECTRODES = ('Fp1', 'F3', 'C3', 'P3', 'O1', 'F7', 'T3', 'T5', 'Fz', 'Cz', 'Pz',
+              'Fp2', 'F4', 'C4', 'P4', 'O2', 'F8', 'T4', 'T6')
+BIPOLAR_DBANANA = ('Fp1-F3', 'F3-C3', 'C3-P3', 'P3-O1', 'Fp1-F7', 'F7-T3', 'T3-T5', 'T5-O1', 'Fz-Cz', 'Cz-Pz',
+                   'Fp2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'Fp2-F8', 'F8-T4', 'T4-T6', 'T6-O2')
+FS = 256
+
+
+def _getChannelIndices(channels: list[str], electrodes: tuple[str] = ELECTRODES) -> list[int]:
+    """Finds the index of each electrode in the list of channels
+
+    Args:
+        channels (list[str]): channels to extract indices
+        electrodes (tuple[str]): electrodes to find. Defaults to the 19 electrodes of the 10-20 system.
+
+    Raises:
+        ValueError: raised if the electrode is not in the list of channels.
+
+    Returns:
+        list[int]: List containing the indices to mathc the list of electrodes to the list of channels
+    """
+    chIndices = list()
+    for electrode in electrodes:
+        found = False
+
+        # Some channel have diffrent names in different datasets.
+        if electrode == 'T3' or electrode == 'T7':
+            electrode = '(T3|T7)'
+        elif electrode == 'T4' or electrode == 'T8':
+            channel = '(T4|T8)'
+        elif electrode == 'T5' or electrode == 'P7':
+            electrode = '(T5|P7)'
+        elif electrode == 'T6' or electrode == 'P8':
+            electrode = '(T6|P8)'
+
+        for i, channel in enumerate(channels):
+            # Regex on channel name
+            if re.search(r'(EEG )?{}(-REF)?'.format(electrode), channel, flags=re.IGNORECASE):
+                found = True
+                break
+        if found:
+            chIndices.append(i)
+        else:
+            raise ValueError("Electrode {} was not found in EDF file".format(electrode))
+    return chIndices
+
+
+def loadEdf(edfFile: str, electrodes: tuple[str] = ELECTRODES, targetFs: int = 256, ref: str = 'Cz') -> np.ndarray:
+    """Load an EDF file and return a numpy array corresponding to provided list of electrodes and sample frequency and
+    re-referenced to Cz
+
+    Args:
+        edfFile (str): path to the EDF file
+        electrodes (tuple[str]): list of electrodes to load. Defaults to the 19 electrodes of the 10-20 system.
+        targetFs (int, optional): sampling frequency. Defaults to 256.
+        ref (str, optional): reference electrode, should be in the list of electrodes (for a unipolar montage) or the string
+                             'bipolar-dBanana' (for a double banana bipolar montage). Defaults to 'Cz'.
+
+    Raises:
+        ValueError: raised if ref is neither in the list of electrodes (unipolar) nor the string 'bipolar-dBanana' (bipolar).
+
+    Returns:
+        np.ndarray: 2D array of data [channels, samples]
+    """
+    with pyedflib.EdfReader(edfFile) as edf:
+        # Get channel indices
+        channels = edf.getSignalLabels()
+        chIndices = _getChannelIndices(channels, electrodes)
+
+        # Get Fs
+        fs = edf.getSampleFrequencies()
+
+        # Read data & resample
+        eegData = list()
+        for i in chIndices:
+            chData = edf.readSignal(i)
+            # Resample
+            if fs[i] != targetFs:
+                chData = resampy.resample(chData, fs[i], targetFs)
+            eegData.append(chData)
+        edf._close()
+
+        # Re-reference
+        eegData = np.array(eegData)
+        # Unipolar
+        if ref in electrodes:
+            refI = electrodes.index(ref)
+            eegData -= eegData[refI]
+        elif ref == 'bipolar-dBanana':
+            # Create reReferencing matrix
+            reRefMatrix = np.zeros((len(BIPOLAR_DBANANA), len(electrodes)))
+            for i, pair in enumerate(BIPOLAR_DBANANA):
+                elecs = pair.split('-')
+                reRefMatrix[i, electrodes.index(elecs[0])] = 1
+                reRefMatrix[i, electrodes.index(elecs[1])] = -1
+            eegData = reRefMatrix @ eegData  # matrix multiplication
+        else:
+            raise ValueError("ref should either be one electrode (for a unipolar montage) or the string 'bipolar-dBanana' "
+                             "(for a double banana bipolar montage).")
+
+    return eegData
+
+
+def standadizeToEdf(edfFile: str, outFile: str, electrodes: tuple[str] = ELECTRODES, fs: int = 256, ref: str = 'Cz'):
+    """Standardize an EDF file.
+
+    Args:
+        edfFile (str): path to the input EDF file
+        outFile (str): path to the output EDF file
+        electrodes (tuple[str]): list of electrodes to load. Defaults to the 19 electrodes of the 10-20 system.
+        fs (int, optional): sampling frequency. Defaults to 256.
+        ref (str, optional): reference electrode, should be in the list of electrodes (for a unipolar montage) or the string
+                             'bipolar-dBanana' (for a double banana bipolar montage). Defaults to 'Cz'.
+
+    Raises:
+        ValueError: raised if ref is neither in the list of electrodes (unipolar) nor the string 'bipolar-dBanana' (bipolar).
+    """
+    eegData = loadEdf(edfFile, electrodes, fs, ref)
+
+    # Build headers
+    with pyedflib.EdfReader(edfFile) as edf:
+        # Get channel indices
+        channels = edf.getSignalLabels()
+        chIndices = _getChannelIndices(channels, electrodes)
+
+        signalHeaders = list()
+        for i in range(len(eegData)):
+            signalHeader = edf.getSignalHeader(chIndices[i])
+            if ref in electrodes:
+                signalHeader['label'] = electrodes[i]
+            elif ref == 'bipolar-dBanana':
+                signalHeader['label'] = BIPOLAR_DBANANA[i]
+            else:
+                raise ValueError("ref should either be one electrode (for a unipolar montage) or the string 'bipolar-dBanana'"
+                                 " (for a double banana bipolar montage).")
+            signalHeader['sample_frequency'] = fs
+            signalHeader['sample_rate'] = signalHeader['sample_frequency']
+            # Check for phys_maxima
+            if np.max(eegData[i]) > signalHeader['physical_max'] or np.min(eegData[i]) < signalHeader['physical_min']:
+                newMax = np.ceil(np.max(np.abs(eegData[i])))
+                signalHeader['physical_min'] = -1 * newMax
+                signalHeader['physical_max'] = newMax
+            signalHeaders.append(signalHeader)
+
+        recordingHeader = edf.getHeader()
+        edf._close()
+
+    # Create directory for file
+    os.makedirs(os.path.dirname(outFile), exist_ok=True)
+    # Write new EDF file
+    pyedflib.highlevel.write_edf(outFile, eegData, signalHeaders, recordingHeader)
+
+
+def standardizeDatasetToEdf(rootDir: str, outDir: str, electrodes: tuple[str] = ELECTRODES, fs: int = 256, ref: str = 'Cz'):
+    """Converts a full dataset to the standard EEG format.
+
+    Args:
+        rootDir (str): root directory of the original dataset
+        outDir (str): root directory of the converted dataset. The data structure of the original dataset is preserved.
+        electrodes (tuple[str], optional): list of electrodes to load. Defaults to the 19 electrodes of the 10-20 system.
+        fs (int, optional): sampling frequency. Defaults to 256.
+        ref (str, optional): reference electrode, should be in the list of electrodes (for a unipolar montage) or the string
+                             'bipolar-dBanana' (for a double banana bipolar montage). Defaults to 'Cz'.
+    """
+    edfFiles = glob.iglob(os.path.join(rootDir, '**/*.edf'), recursive=True)
+    for edfFile in edfFiles:
+        outFile = os.path.join(outDir, edfFile[len(rootDir):])  # swap rootDir for outDir
+        standadizeToEdf(edfFile, outFile, electrodes, fs, ref)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='EDF format standardization',
+        description="Converts EDF files in a referential montage to a standardized list of electrodes and channel names. "
+                    "Two formats are allowed : either conversion of full datasets by providing a directory name as input and "
+                    "output or conversion of individual files by providing an edf file as input out output"
+    )
+    parser.add_argument('input',
+                        help="either the root directory of the original dataset or the input edf file.")
+    parser.add_argument('output',
+                        help="either the root directory of the converted dataset or the output edf file."
+                             " The data structure of the original dataset is preserved.")
+    parser.add_argument('-e', '--electrodes',
+                        help="List of electrodes. These should be provided as comma-separated-values. By default uses"
+                        " the 19 electrodes of the 10-20 system.")
+    parser.add_argument('-f', '--fs',
+                        help="Sampling frequency for the converted data. Defaults to 256 Hz.", type=int, default=256)
+    parser.add_argument('-r', '--ref',
+                        help="Reference for the converted data. Should be in the list of electrodes (for a unipolar montage) "
+                             "or the string 'bipolar-dBanana' (for a double banana bipolar montage). Defaults to 'Cz'",
+                        default='Cz')
+    args = parser.parse_args()
+
+    # Convert list of electrodes to list
+    if args.electrodes is not None:
+        args.electrodes = args.electrodes.split(',')
+    else:
+        args.electrodes = ELECTRODES
+
+    if os.path.isdir(args.input):
+        standardizeDatasetToEdf(args.input, args.output, args.electrodes, args.fs, args.ref)
+    else:
+        standadizeToEdf(args.input, args.output, args.electrodes, args.fs, args.ref)
